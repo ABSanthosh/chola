c1fc9759e58807f69fdbe9cb0fc386da59d92e44
ABSanthosh <a.b.santhosh02@gmail.com>
2026-05-27 21:43:17 -0700

    purge

--- src/utils/Debug.enums.ts
+++
@@ -1,8 +0,0 @@
-export const DEBUG = {
-  COLOR: {
-    BLUE: [0, 0, 1] as [number, number, number],
-    GREEN: [0, 1, 0] as [number, number, number],
-    RED: [1, 0, 0] as [number, number, number],
-    YELLOW: [1, 1, 0] as [number, number, number],
-  },
-};
